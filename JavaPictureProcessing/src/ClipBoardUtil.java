669a508d291b765685152582eb4d6c2144da28c5
xuleichao <1205448156@qq.com>
2017-12-13 17:26:53 +0800

    stringtoclipboard

---
+++ JavaPictureProcessing/src/ClipBoardUtil.java
@@ -0,0 +1,15 @@
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+
+public class ClipBoardUtil{
+    public static void main(String[] args){
+        setSysClipboardText("dsdfsfsdf");
+    }
+    public static void setSysClipboardText(String writeMe){  
+        Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();  
+        Transferable tText = new StringSelection(writeMe);  
+        clip.setContents(tText, null);  
+    } 
+}
