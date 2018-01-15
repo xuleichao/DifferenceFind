c9903e6a92084a22f1efe00b828dee7dbc7c5b97
xuleichao <1205448156@qq.com>
2018-01-15 23:02:37 +0800

    smallstep

--- HelloWorld/src/ClipBoardUtil.java
+++ HelloWorld/src/ClipBoardUtil.java
@@ -21,7 +21,7 @@ import javax.swing.text.html.HTMLDocument.Iterator;
 public class ClipBoardUtil{
     public static void main(String[] args) throws Exception{
     	cutPic("test.png", "result_test_1.jpg", 513, 0, 1024, 768);
-		cutPic("test.png", "result_test_2.jpg", 513, 0, 1024, 768);
+		cutPic("test.png", "result_test_2.jpg", 0, 0, 512, 768);
     	Image im;
     	ByteArrayOutputStream out = new ByteArrayOutputStream();
     	System.out.println("我们开始做对比");
